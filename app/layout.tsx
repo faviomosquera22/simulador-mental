d155f2944b8773d888cd53e1372bda3c874ac8dc
faviomosquera22 <113486107+faviomosquera22@users.noreply.github.com>
2026-03-23 10:04:32 -0500

    Add Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: {
@@ -23,6 +24,7 @@ export default function RootLayout({
             {children}
           </div>
         </div>
+        <SpeedInsights />
       </body>
     </html>
   );
